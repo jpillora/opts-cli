9cd53874b56acade96f583543aa1821765827e13
Jaime Pillora <dev@jpillora.com>
2019-05-19 19:25:28 +1000

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jpillora/opts-cli
+
+go 1.12
+
+require github.com/jpillora/opts v1.0.0
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jpillora/opts"
+	"github.com/jpillora/opts-cli/internal/initopts"
+)
+
+var (
+	Version string = "dev"
+	Date    string = "na"
+	Commit  string = "na"
+)
+
+type root struct {
+	parsedOpts opts.ParsedOpts
+}
+
+func main() {
+	r := root{}
+	ro := opts.New(&r).
+		EmbedGlobalFlagSet().
+		Complete().
+		Version(Version)
+
+	initopts.Register(ro)
+
+	r.parsedOpts = ro.Parse()
+	err := r.parsedOpts.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "run error %v\n", err)
+		os.Exit(2)
+	}
+}
+
+func (rt *root) Run() {
+	fmt.Printf("Version: %s\nDate: %s\nCommit: %s\n", Version, Date, Commit)
+	fmt.Println(rt.parsedOpts.Help())
+}
